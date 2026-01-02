c394a80c63f27a8c3b3526ee8aece432877b01f4
webioann <webioann@gmail.com>
2026-01-02 12:02:04 +0200

    TEST

--- types/auth.types.ts
+++
@@ -1,8 +0,0 @@
-import { SetStateAction } from 'react'
-
-export interface IAuthButtonProps {
-    email: string;
-    password: string;
-    setWarning: (param: boolean) => void;
-    setError: React.Dispatch<SetStateAction<string | null>>
-}
